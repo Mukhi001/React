326393adda5b9e3f1c900b846ae288db63cdc578
Mukhi001 <mukeshgongada1024@gmail.com>
2024-09-05 13:20:56 +0530

    21-aug-tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import Cardd from "./components/card"
 import NavBar from "./components/nav";
-import { ClassComponent, TableComponent } from "./classComp/classComp";
+
 import DefaultCarousel from "./components/carousel";
 import Footer from "./components/footer";
 import { InstagramLoader } from "./components/instaconload";
 import { GitIcon } from "./components/devicons";
-import RecipeList from "./Recipe/recipe";
-import { FakeStore } from "./Recipe/fakestore";
-import Counter from "./components/Hooks";
-import { DynamicTable } from "./Tasks/task1";
-import { BsLightbulb } from "react-icons/bs";
-import Todo from "./hooks/setState";
 function App() {
-  
   return (
     <>
- <Counter/>
- <Todo/>
+      <NavBar />
+      <DefaultCarousel />
+      <div style= {{display:"flex", flexWrap:"wrap", justifyContent:"space-evenly",padding:"4px"}}>
+      <Cardd/>
+      <Cardd/>
+      <Cardd/>
+      <Cardd/>
+      </div>
+      <GitIcon/>
+      <InstagramLoader/>
+      <Footer/>
     </>
-   
   );
 }
 
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,44 +2,23 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Spinn } from './spinn';
 import Toast from './toast';
-import { createTodo } from "../jsHelpers/data"
 
-function Cardd(props) {
-  const {id,text,onClick,checker}=props;
-  
+function Cardd() {
   return (
     <Card style={{ width: '18rem',border:'2px solid' }}>
-      <Card.Img variant="top"/>
+      <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>{id}</Card.Title>
+        <Card.Title>Card Title</Card.Title>
         <Spinn/>
         <Card.Text>
-         {text}
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
         </Card.Text>
-       {/* <Toast/> */}
-        <Button variant="primary" onClick={onClick}>delete</Button>
-        <Button variant="primary" onClick={checker}>even/odd</Button>
+       <Toast/>
+        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
 }
 
 export default Cardd;
-
-
-export const BasicCard =({title,description,value,ind,removeHandler})=>{
-
-  return (
-    <Card style={{ width: '18rem',margin:'8px' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {description}
-        </Card.Text>
-        <Button variant="primary" onClick={()=>removeHandler(ind)}>{value}</Button>
-      </Card.Body>
-    </Card>
-  );
-
-}
